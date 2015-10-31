883bffa0867a394e4b474b6fdd33b608e8a98cc0
Daniel Koics <koics.dani@gmail.com>
2015-10-31 22:43:08 +0100

    roman_numerals: RomanDigit class written and being tested by unit-test terrible problems with assertRaises

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -1,81 +1,147 @@
 __author__ = 'KoicsD'
 
 
-class Digit():
+class IncrementError(Exception):
+    pass
+
+
+class RomanDigit:
 
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    signs = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+    increase_limits = {1: 2, 5: 3}
+
+    def get_increase_limit(self):
+        return RomanDigit.increase_limits[int(str(self.value)[0])]
+
+    def is_possible_increment(self, incr_value: int):
+        stringised = str(self.value)
+        how_many = int(stringised[0])                                   # unnecessary
+        unit = int('1' + stringised[1:])
+        assert self.value == how_many * unit, "something went wrong in 'is_possible_increment'"
+        return incr_value in [unit, 0]
+
+    def is_possible_decrement(self, decr_value: int):
+        stringised = str(self.value)
+        how_many = int(stringised[0])
+        unit = int('1' + stringised[1:])
+        assert self.value == how_many * unit, "something went wrong in 'is_possible_decrement'"
+        if how_many == 1:
+            return decr_value in [unit // 10, 0]
+        elif how_many == 5:
+            return decr_value in [unit, 0]
+
+    def __init__(self, data):
+        if data == 0 or data == '':
+            self.value = 0
+            self.sign = ''
+        elif type(data) == str:
+            if data not in RomanDigit.values:
+                raise ValueError("'{0}' is not a valid string for constructing a 'RomanDigit' object".format(data))
+            self.sign = data
+            self.value = RomanDigit.values[data]
+        elif type(data) == int:
+            if data not in RomanDigit.signs:
+                raise ValueError("{0} is not a valid value for constructing a 'RomanDigit' object".format(data))
+            self.value = data
+            self.sign = RomanDigit.signs[data]
+            assert self.sign == [item for item in RomanDigit.values if RomanDigit.values[item] == data][0],\
+                "error in __init__"  # inv(dict[data]) == key
+        else:
+            raise TypeError("{0} is not a suitable type for constructing a 'RomanDigit' object.".format(type(data)))
+        self.increment = None
+        self.decrement = None
+        self.increase_order = 0
+
+    def increase(self, data, forced=False):
+        if type(data) == RomanDigit:
+            new_digit = data
+        else:
+            new_digit = RomanDigit(data)
+        if not self.is_possible_increment(new_digit.get_value()):
+            raise ValueError("{0} cannot be increased with {1} within the same decimal digit".format(self.sign,
+                                                                                                     new_digit.sign))
+        if self.decrement is not None:
+            if forced:
+                self.dispose_decrement()
+            else:
+                raise IncrementError("{0} cannot be increased without disposing its decrement".format(self.get_sign()))
+        if self.increase_order == self.get_increase_limit():
+            assert self.increment is not None, "'increment_order' is not 0 but 'increment' is None"     # assert
+            raise IncrementError("{0} cannot be further increased".format(self.get_sign()))
+        if self.increment is None:
+            self.increment = new_digit
+        else:
+            self.increment.increase(new_digit)
+        self.increase_order += 1
+
+    def dispose_increment(self, all_increment=False):
+        if all_increment:
+            self.increment = None
+            self.increase_order = 0
+            return
+        if self.increment is None:
+            assert self.increase_order == 0, "'increment' is None but 'increase_order' is not 0"        # assert
+            raise IncrementError("{0} has no increment to dispose".format(self.get_sign()))
+        try:
+            self.increment.dispose_increment()
+        except IncrementError:
+            self.increment = None
+        assert self.increase_order > 0, "'increment' is not None but 'increase_order' is 0 or less"     # assert
+        self.increase_order -= 1
+
+    def decrease(self, data, forced=False):
+        if type(data) == RomanDigit:
+            new_digit = data
+        else:
+            new_digit = RomanDigit(data)
+        if not self.is_possible_decrement(new_digit.get_value()):
+            raise ValueError("{0} cannot be decreased with {1} within the same decimal digit".format(self.sign,
+                                                                                                     new_digit.sign))
+        if self.increase_order > 0:
+            assert self.increment is not None, "'increment_order' is not 0 but 'increment' is None"     # assert
+            if forced:
+                self.dispose_increment()
+            else:
+                raise IncrementError("{0} cannot be decreased without disposing its increment".format(self.get_sign()))
+        if self.decrement is not None:
+            raise IncrementError("{0} cannot be further decreased".format(self.get_sign()))
+        self.decrement = new_digit
+
+    def dispose_decrement(self):
+        if self.decrement is None:
+            raise IncrementError("{0} has no decrement to dispose".format(self.get_sign()))
+        self.decrement = None
+
+    def get_value(self):
+        ret = self.value
+        if self.decrement is not None:
+            ret -= self.decrement.get_value()
+        elif self.increment is not None:
+            ret += self.increment.get_value()
+        return ret
+
+    def get_sign(self):
+        ret = self.sign
+        if self.decrement is not None:
+            ret = self.decrement.get_sign() + ret
+        elif self.increment is not None:
+            ret += self.increment.get_sign()
+        return ret
 
 
-    def __init__(self):
-        self.increaser = None
-        self.decreaser = None
-        self.increaser_order = 0
-        self.value = 0
-        self.sign = ''
+class DecimalDigit:
+    pass
 
-class One(Digit):
 
-    def __init__(self):
-        self.increaser = None
-        # 1 cannot have any decreaser
-        self.increaser_order = 0
-        self.value = 1
-        self.sign = 'I'
+class RomanNumeral:
+    pass
 
-class Five(Digit):
 
-    def __init__(self):
-        self.increaser = None
-        self.decreaser = None
-        self.increaser_order = 0
-        self.value = 5
-        self.sign = 'V'
+def main(*args):
+    # script-body comes here
+    pass
 
-class Ten(Digit):
-
-    def __init__(self):
-        self.increaser = None
-        self.decreaser = None
-        self.increaser_order = 0
-        self.value = 10
-        self.sign = 'X'
-
-class Fifty(Digit):
-
-    def __init__(self):
-        self.increaser = None
-        self.decreaser = None
-        self.increaser_order = 0
-        self.value = 50
-        self.sign = 'L'
-
-class OneHundred(Digit):
-
-    def __init__(self):
-        self.increaser = None
-        self.decreaser = None
-        self.increaser_order = 0
-        self.value = 100
-        self.sign = 'C'
-
-class FiveHundred(Digit):
-
-    def __init__(self):
-        self.increaser = None
-        self.decreaser = None
-        self.increaser_order = 0
-        self.value = 500
-        self.sign = 'D'
-
-class OneThousand(Digit):
-
-    def __init__(self):
-        self.increaser = None
-        self.decreaser = None
-        self.increaser_order = 0
-        self.value = 1000
-        self.sign = 'M'
-
-values = {'I': One, 'V': Five, 'X': Ten, 'L': Fifty, 'C': OneHundred, 'D': FiveHundred, 'M': OneThousand}
-
-my_obj = values['V']()
+if __name__ == "__main__":
+    from sys import argv
+    main(*argv)
--- roman_numerals/roman_numerals_test.py
+++ roman_numerals/roman_numerals_test.py
@@ -1,12 +1,276 @@
 __author__ = 'KoicsD'
 
 import unittest
-import roman_numerals.roman_numerals as my_module
+import roman_numerals as my_module
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    values = [1, 5, 10, 50, 100, 500, 1000]
+    signs = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+    increment_values = [1, 1, 10, 10, 100, 100, 1000]
+    increment_signs = ['I', 'I', 'X', 'X', 'C', 'C', 'M']
+    max_increment_nums = [2, 3, 2, 3, 2, 3, 2]
+    decrement_values = [0, 1, 1, 10, 10, 100, 100]
+    decrement_signs = ['', 'I', 'I', 'X', 'X', 'C', 'C']
 
-        self.assertEqual(True, False)
+    def test_RomanDigit_TypeError_for_float(self):
+
+        self.assertRaises(TypeError, my_module.RomanDigit, 5.5)
+
+    def test_RomanDigit_ValueError_for_wrong_int(self):
+        self.assertRaises(ValueError, my_module.RomanDigit, 6)
+
+    def test_RomanDigit_ValueError_for_wrong_letter(self):
+        self.assertRaises(ValueError, my_module.RomanDigit, 'S')
+
+    def test_RomanDigit_for_int(self):
+        # tests if all RomanDigits can be constructed from it's value
+        # also tests if these digits knows their own increase-limits
+        for i in range(len(MyTestCase.values)):
+            v = MyTestCase.values[i]
+            s = MyTestCase.signs[i]
+            my_obj = my_module.RomanDigit(v)
+            self.assertEqual(my_obj.get_value(), v, 'value is not all-right (value: {0}, sign: {1})'.format(v, s))
+            self.assertEqual(my_obj.get_sign(), s, 'sign is not all-right (value: {0}, sign: {1})'.format(v, s))
+            self.assertEqual(my_obj.get_increase_limit(), MyTestCase.max_increment_nums[i],
+                             'sign is not all-right (value: {0}, sign: {1})'.format(v, s))
+
+    def test_RomanDigit_for_str(self):
+        # tests if all RomanDigits can be constructed from it's sign
+        # also tests if these digits knows their own increase-limits
+        for i in range(len(MyTestCase.signs)):
+            v = MyTestCase.values[i]
+            s = MyTestCase.signs[i]
+            my_obj = my_module.RomanDigit(s)
+            self.assertEqual(my_obj.get_value(), v, 'value is not all-right (value: {0}, sign: {1})'.format(v, s))
+            self.assertEqual(my_obj.get_sign(), s, 'sign is not all-right (value: {0}, sign: {1})'.format(v, s))
+            self.assertEqual(my_obj.get_increase_limit(), MyTestCase.max_increment_nums[i],
+                             'sign is not all-right (value: {0}, sign: {1})'.format(v, s))
+
+    def test_RomanDigit_increment(self):
+        # Tests all the increaser and decreaser functions of RomanDigits.
+        # Covers all the 'round' numbers.
+        # Checks errors that should be raised when trying to do something impossible or suspicious.
+
+        for i in range(7):
+            # initializing base value:
+            base_value = MyTestCase.values[i]
+            base_sign = MyTestCase.signs[i]
+            my_obj = my_module.RomanDigit(base_value)
+            v = base_value
+            s = base_sign
+
+            # initializing increaser-test:
+            increment_value = MyTestCase.increment_values[i]
+            increment_sign = MyTestCase.increment_signs[i]
+            max_n_increments = MyTestCase.max_increment_nums[i]
+
+            msg = "'V' |--> 'VI' |--> 'VII' |--> 'VIII' ie. 'X' |--> 'XX' |--> 'XXX' etc."
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, False, max_n_increments, msg)
+
+            msg = "'VIII' |--> 'VIIII' ie. 'XXX' |--> 'XXXX' etc. impossible"
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, False, 0, msg)
+            msg = "'VIII' |--> 'VIIII' ie. 'XXX' |--> 'XXXX' etc. impossible even with forced increase"
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, True, 0, msg)
+
+            msg = "discarding last increment ('VIII' |--> 'VI' ie. 'XXX' |--> 'XX' etc.)"
+            [my_obj, v, s] = self.dispose_increment(base_value, base_sign,
+                                                    my_obj, v, s,
+                                                    increment_value, increment_sign, False, True, msg)
+
+            msg = "one increment again ('VII' |--> 'VIII' ie. 'XX' |--> 'XXX' etc.)"
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, False, 1, msg)
+
+            msg = "discarding all increments ('VIII' |--> 'V' ie. 'XXX' |--> 'X' etc.)"
+            [my_obj, v, s] = self.dispose_increment(base_value, base_sign,
+                                                    my_obj, v, s,
+                                                    increment_value, increment_sign, True, True, msg)
+
+            msg = "'V' |--> ... |--> 'VIII' ie. 'X' |--> ... |--> 'XXX' etc. again"
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, False, max_n_increments, msg)
+
+            msg = "discarding all increments again ('VIII' |--> 'V' ie. 'XXX' |--> 'X' etc.)"
+            [my_obj, v, s] = self.dispose_increment(base_value, base_sign,
+                                                    my_obj, v, s,
+                                                    increment_value, increment_sign, True, True, msg)
+
+            msg = "testing forced increment('V' |--> ... |--> 'VIII' ie. 'X' |--> ... |--> 'XXX' etc. again)"
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, True, max_n_increments, msg)
+
+            # initializing decreaser-test:
+            decrement_value = MyTestCase.decrement_values[i]
+            decrement_sign = MyTestCase.decrement_signs[i]
+
+            msg = "'VIII' |--> 'IV' ie. 'XXX' |--> 'IX' etc. directly impossible"
+            [my_obj, v, s] = self.decrease(base_value, base_sign,
+                                           my_obj, v, s,
+                                           decrement_value, decrement_sign, False, False, msg)
+
+            msg = "'VIII' |--> 'IV' ie. 'XXX' |--> 'IX' etc. directly yet possible with forced decreasing"
+            [my_obj, v, s] = self.dispose_decrement(base_value, base_sign,
+                                                    my_obj, v, s,
+                                                    decrement_value, decrement_sign, True, msg)
+
+            msg = "'IV' |--> 'IIV' ie. 'IX' |--> 'IIX' etc. impossible"
+            [my_obj, v, s] = self.decrease(base_value, base_sign,
+                                           my_obj, v, s,
+                                           decrement_value, decrement_sign, False, False, msg)
+            msg = "'IV' |--> 'IIV' ie. 'IX' |--> 'IIX' etc. impossible even if forced"
+            [my_obj, v, s] = self.decrease(base_value, base_sign,
+                                           my_obj, v, s,
+                                           decrement_value, decrement_sign, True, False, msg)
+
+            msg = "discarding decrement ('IV' |--> 'V' ie. 'IX' |--> 'X')"
+            [my_obj, v, s] = self.dispose_decrement(base_value, base_sign,
+                                                    my_obj, v, s,
+                                                    decrement_value, decrement_sign, True, msg)
+
+            msg = "'V' |--> 'IV' ie. 'X' |--> 'IX'"
+            [my_obj, v, s] = self.decrease(base_value, base_sign,
+                                           my_obj, v, s,
+                                           decrement_value, decrement_sign, False, True, msg)
+
+            msg = "discarding decrement ('IV' |--> 'V' ie. 'IX' |--> 'X') again"
+            [my_obj, v, s] = self.dispose_decrement(base_value, base_sign,
+                                                    my_obj, v, s,
+                                                    decrement_value, decrement_sign, True, msg)
+
+            msg = "testing forced decrement ('V' |--> 'IV' ie. 'X' |--> 'IX' again)"
+            [my_obj, v, s] = self.decrease(base_value, base_sign,
+                                           my_obj, v, s,
+                                           decrement_value, decrement_sign, True, True, msg)
+
+            # switching back to increasing:
+
+            msg = "'IV' |--> 'VI' ie. 'IX' |--> 'XX' directly impossible"
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, False, 0, msg)
+
+            msg = "'IV' |--> 'VI' ie. 'IX' |--> 'XX' directly yet possible with forced increase"
+            [my_obj, v, s] = self.increase(base_value, base_sign,
+                                           my_obj, v, s,
+                                           increment_value, increment_sign, True, 1, msg)
+
+    def increase(self, base_val: int, base_sign: str,
+                  base_obj: my_module.RomanDigit, v: int, s: str,
+                  incr_val: int, incr_sign: str, forced: bool, max_n_incr: int, msg: str):
+        # base_val: the base value of the RomanDigit being tested
+        # base_sign: the base sign of the RomanDigit being tested
+        # base_obj: the RomanDigit to be tested
+        # v: the value for incrementing, varies through the loop
+        # s: the sign for incrementing, varies through the loop
+        # incr_val: the value for calling the increaser function
+        # incr_sign: the sign for calling the increaser function
+        # forced: decides whether to use forced increment
+        # max_n_incr: the number of increments -- failure is asserted if zero
+        # msg: message to be displayed on assertion failure
+        #
+        # return values: the RomanDigit and the value and sign after the test
+
+        if max_n_incr == 0:
+            self.assertRaises(my_module.IncrementError, my_module.RomanDigit.increase, base_obj, incr_val, forced)
+        for j in range(max_n_incr):
+            v += incr_val
+            s += incr_sign
+            base_obj.increase(incr_val, forced)
+            self.assertEqual(base_obj.get_value(), v,
+                             'value is not all-right (value: {0}, sign: {1})\nmessage: %s'.format(v, s) % msg)
+            self.assertEqual(base_obj.get_sign(), s,
+                             'sign is not all-right (value: {0}, sign: {1})\nmessage: %s'.format(v, s) % msg)
+        return [base_obj, v, s]
+
+    def dispose_increment(self, base_val: int, base_sign: str,
+                          base_obj: my_module.RomanDigit, v: int, s: str,
+                          incr_val: int, incr_sign: str, all_increments: bool, assert_success: bool, msg: str):
+        # base_val: the base value of the RomanDigit being tested
+        # base_sign: the base sign of the RomanDigit being tested
+        # base_obj: the RomanDigit to be tested
+        # v: the value for incrementing, varies through the loop
+        # s: the sign for incrementing, varies through the loop
+        # all_increments: decides whether to dispose all increments
+        # assert_success: decides whether to assert success
+        # msg: message to be displayed on assertion failure
+        #
+        # return value: the RomanDigit and the value and sign after the test
+
+        if assert_success:
+            if all_increments:
+                v = base_val
+                s = s[1]
+            else:
+                v -= incr_val
+                s = s[:-1]
+            base_obj.dispose_increment(all_increments)
+            self.assertEqual(base_obj.get_value(), v,
+                             'value is not all-right (value: {0}, sign: {1})\nmessage: %s'.format(v, s) % msg)
+            self.assertEqual(base_obj.get_sign(), s,
+                             'sign is not all-right (value: {0}, sign: {1})\nmessage: %s'.format(v, s) % msg)
+        else:
+            self.assertRaises(my_module.IncreaseError, my_module.RomanDigit.dispose_increment, base_obj, all_increments)
+        return [base_obj, v, s]
+
+    def decrease(self, base_val: int, base_sign: str,
+                  base_obj: my_module.RomanDigit, v: int, s: str,
+                  decr_val: int, decr_sign: str, forced: bool, assert_success: bool, msg: str):
+        # base_val: the base value of the RomanDigit being tested
+        # base_sign: the base sign of the RomanDigit being tested
+        # base_obj: the RomanDigit to be tested
+        # v: the value for decrementing, varies through the loop
+        # s: the sign for decrementing, varies through the loop
+        # decr_val: the value for calling the decreaser function
+        # decr_sign: the sign for calling the decreaser function
+        # forced: decides whether to use forced decrement
+        # assert_success: decides whether to assert success
+        #
+        # return values: the RomanDigit and the value and sign after the test
+
+        if assert_success:
+            v -= decr_val
+            s = decr_sign + s
+            base_obj.decrease(decr_val, forced)
+            self.assertEqual(base_obj.get_value(), v, 'bla')
+            self.assertEqual(base_obj.get_sign(), s, 'bla')
+        else:
+            self.assertRaises(my_module.IncreaseError, my_module.RomanDigit.decrease, base_obj, decr_val)
+        return [base_obj, v, s]
+
+    def dispose_decrement(self, base_val: int, base_sign: str,
+                  base_obj: my_module.RomanDigit, v: int, s: str,
+                  decr_val: int, decr_sign: str, assert_success: bool, msg: str):
+        # base_val: the base value of the RomanDigit being tested
+        # base_sign: the base sign of the RomanDigit being tested
+        # base_obj: the RomanDigit to be tested
+        # v: the value for decrementing, varies through the loop
+        # s: the sign for decrementing, varies through the loop
+        # decr_val: the value for calling the decreaser function
+        # decr_sign: the sign for calling the decreaser function
+        # forced: decides whether to use forced decrement
+        # assert_success: decides whether to assert success
+        #
+        # return values: the RomanDigit and the value and sign after the test
+
+        if assert_success:
+            v += decr_val
+            assert v == base_val and s[-1] == s[1:], "something went wrong with test\nmessage: %s" % msg
+            s = s[1:]
+            base_obj.dispose_decrement()
+            self.assertEqual(base_obj.get_value(), v, 'value is not all-right\nmessage: %s' % msg)
+            self.assertEqual(base_obj.get_sign(), s, 'sign is not all-right\nmessage: %s' % msg)
+        else:
+            self.assertRaises(my_module.IncreaseError, my_module.RomanDigit.dispose_decrement, base_obj)
+        return [base_obj, v, s]
 
 
 if __name__ == '__main__':
